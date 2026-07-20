bf2b4d3fbe4bf412d79c3a28c01ac1c5785382d2
strawberryskeleton <xradhi5685@gmail.com>
2026-07-21 00:52:37 +0530

    added start screen and end screen logic + debugging it ++ fixed some other logics to fit start/end screen system

--- main.js
+++ main.js
@@ -3,10 +3,14 @@ const you = document.getElementById('you')
 const cmdBox = document.getElementById('cmd-box')
 const highscoreDisplay = document.getElementById('highscore')
 const scoreDisplay = document.getElementById('score')
+const startScreen = document.getElementById('start-screen')
+const endScreen = document.getElementById('end-screen')
+const endScoreDisplay = document.getElementById('end-score')
+const startGameBtns = document.querySelectorAll('.start-game-btn')
 
-const commands = ["run", "sit",  "dance", "eat"]
+const commands = ["run", "sit", "dance", "eat"]
 
-let isGameActive = true
+let isGameActive = false
 let isJump = false
 let isJumping = false
 let cmdInterval
@@ -14,39 +18,53 @@ let cmdInterval
 let highscore = Number(localStorage.getItem('highscore')) || 0
 let score = 0
 
-highscoreDisplay.textContent = String(highscore).padStart(2, '0')
-scoreDisplay.textContent = String(score).padStart(2, '0')
 
-// if (isGameActive) {
-//     cmdInterval = setInterval(() => {
-//         isJump = false
-//         getRandomAction()
-//         // cmdBox.textContent = "..."
-//         let reactionTimeout = setTimeout(() => {
-//             evalUserAction()
-//             cmdBox.textContent = "...."
-//         }, 1000)
-//     }, 3000)
-// }
+
+startGameBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        startGame()
+        // isGameActive = true
+    })
+})
+
+
+// console.log("here")
+
+    // cmdInterval = setInterval(() => {
+    //     if (!isGameActive) return
+
+    //     console.log("interval running")
+    //     isJump = false
+    //     getRandomAction()
+    //     // cmdBox.textContent = "..."
+    //     let reactionTimeout = setTimeout(() => {
+    //         evalUserAction()
+    //         cmdBox.textContent = "...."
+    //     }, 1000)
+    // }, 3000)
+
+
 
 document.addEventListener('keypress', (ev) => {
     // console.log(ev.code)
     // getRandomAction()
 
-    you.classList.add('jumping')
-    isJumping = true
+    if (isGameActive && !isJumping) {
+        you.classList.add('jumping')
+        isJumping = true
 
-    // evalUserAction()
+        // evalUserAction()
 
-    const jumpTimeout = setTimeout(() => {
-        you.classList.remove('jumping')
-        isJumping = false
-        // console.log("now")
-    },1000)
+        const jumpTimeout = setTimeout(() => {
+            you.classList.remove('jumping')
+            isJumping = false
+            // console.log("now")
+        }, 1000)
+    }
 
 })
 
-function getRandomAction () {
+function getRandomAction() {
 
     const isJumpGenerated = Math.floor(Math.random() * 2) == 1 ? true : false
 
@@ -63,7 +81,7 @@ function getRandomAction () {
 
 }
 
-function evalUserAction () {
+function evalUserAction() {
     if (isJumping) {
 
         if (isJump) {
@@ -71,12 +89,14 @@ function evalUserAction () {
             increaseScore()
         } else {
             console.log("not ok")
+            gameOver()
         }
 
     }
     else {
         if (isJump) {
             console.log("why not")
+            gameOver()
         } else {
             console.log("fine")
             increaseScore()
@@ -86,13 +106,53 @@ function evalUserAction () {
     console.log("----")
 }
 
-function increaseScore () {
+function increaseScore() {
     score++
     scoreDisplay.textContent = String(score).padStart(2, '0')
 
     if (score >= highscore) {
         highscore = score
         localStorage.setItem('highscore', highscore)
-        highscoreDisplay.textContent = String(highscore).padStart(2,'0')
+        highscoreDisplay.textContent = String(highscore).padStart(2, '0')
     }
+}
+
+function startGame() {
+    isGameActive = true
+    score = 0
+
+    clearInterval(cmdInterval)
+    // clearTimeout(reactionTimeout)
+
+    console.log("game start")
+
+    startScreen.classList.add("hidden")
+    endScreen.classList.add("hidden")
+
+    highscoreDisplay.textContent = String(highscore).padStart(2, '0')
+    scoreDisplay.textContent = String(score).padStart(2, '0')
+
+     cmdInterval = setInterval(() => {
+        if (!isGameActive) return
+        
+        console.log("interval running")
+        isJump = false
+        getRandomAction()
+        // cmdBox.textContent = "..."
+        let reactionTimeout = setTimeout(() => {
+            evalUserAction()
+            cmdBox.textContent = "...."
+        }, 1000)
+    }, 3000)
+
+}
+
+function gameOver() {
+    isGameActive = false
+
+    endScoreDisplay.textContent = String(score).padStart(2, '0')
+    endScreen.classList.remove("hidden")
+
+    clearInterval(cmdInterval)
+    // clearTimeout(reactionTimeout)
 }
